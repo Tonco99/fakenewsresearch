d6dff63ac54fe9cf1bc0d980792351527f75844f
Tonco99 <73219722+Tonco99@users.noreply.github.com>
2021-11-19 11:49:49 +0100

    Delete static directory

--- static/index.js
+++
@@ -1,8 +0,0 @@
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-//     document.getElementById("jumbo").style.height = "300px";
-//   }
-// }
